7b8d2123fbe2b8da6b098de8e2e78fcbfa21d162
astevenson6 <astevenson6@yahoo.com>
2019-06-27 22:42:57 -0700

    pseudocode for giftastic

---
+++ assets/Javascript.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+    // array for topics
+    var topics = ["", "", ""]
+
+    // for loop function to make buttons for each item in this array
+    // set attributes for buttons to make on click animation easier
+
+
+
+    // Call Button Function Here
+
+    // queryUrl defined here
+    // call the API to display 10 static gifs
+    // Animate on click using still/animate
+    // Display Rating of every gif but pulling response.rating
+
+
+
+
+
+
+
+
+
+
+
+
+})
